72b219d436c235d678ecb723330f5df8af3bf436
Joseph Velliah <josephbtech@gmail.com>
2023-08-24 21:28:26 -0500

    added ConversationBufferWindowMemory

--- web-app/app.py
+++ web-app/app.py
@@ -6,7 +6,7 @@ import pinecone
 from langchain.llms import OpenAI
 from langchain.vectorstores import Pinecone
 from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.prompts.prompt import PromptTemplate
 
@@ -24,27 +24,39 @@ pinecone_env = os.getenv("PINECONE_ENV")
 pinecone_index_name = os.getenv("PINECONE_INDEX_NAME")
 
 # Validate environment variables
-if not all([llm_model, open_api_key, pinecone_api_key, pinecone_env, pinecone_index_name]):
+if not all(
+    [llm_model, open_api_key, pinecone_api_key, pinecone_env, pinecone_index_name]
+):
     logger.error("Missing required environment variables.")
     exit(1)
 
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-custom_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. At the end of standalone question add this 'Answer the question in English(USA) language.' If you do not know the answer reply with 'I am sorry'.
+# We set k=n to keep the last n chat interactions in memory.
+memory = ConversationBufferWindowMemory(
+    memory_key="chat_history", return_messages=True, k=5
+)
+custom_template = """
+You are an assistant that provides varied responses and avoids repetition.
+
+Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. At the end of standalone question add this 'Answer the question in English(USA) language.' If you do not know the answer reply with 'I am sorry'.
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:"""
+
 CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
 
 try:
-    llm = OpenAI(openai_api_key=open_api_key, temperature=0)
+    llm = OpenAI(openai_api_key=open_api_key, temperature=0.7)
     embeddings = OpenAIEmbeddings(openai_api_key=open_api_key)
     pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
     vector_db = Pinecone.from_existing_index(pinecone_index_name, embeddings)
+    vector_db_retriever = vector_db.as_retriever()
+    # We set "k": n to get the top n similar documents from pinecone index based on the question asked by the user.
+    vector_db_retriever.search_kwargs = {"k": 5}
     qa = ConversationalRetrievalChain.from_llm(
         llm=llm,
         chain_type="stuff",
-        retriever=vector_db.as_retriever(),
+        retriever=vector_db_retriever,
         condense_question_prompt=CUSTOM_QUESTION_PROMPT,
         return_source_documents=False,
         memory=memory,
@@ -52,6 +64,7 @@ try:
 except Exception as e:
     logger.error(f"Failed to initialize services: {e}")
     exit(1)
+
 
 @app.route("/ask", methods=["POST"])
 def ask():
@@ -62,6 +75,12 @@ def ask():
 
         if not question:
             return jsonify({"error": "Please enter your question."})
+        
+        # Check for specific inputs and provide custom responses
+        if question.lower() in ["thank you", "thanks", "thank you!"]:
+            return jsonify({"answer": "You're welcome!"})
+        elif question.lower() in ["bye", "goodbye", "see you", "farewell"]:
+            return jsonify({"answer": "Goodbye! If you have more questions, feel free to ask."})
 
         ai_response = qa({"question": question})
         return jsonify({"answer": ai_response["answer"]})
@@ -69,9 +88,11 @@ def ask():
         logger.error(f"Failed to process question: {e}")
         return jsonify({"error": "An error occurred while processing your question."})
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
